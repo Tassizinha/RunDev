ea6436fa83f4282607ba5a64a5f7d32c67c875e8
Victoria Tassi <victoriatassi441@gmail.com>
2024-08-28 17:02:22 -0300

    “login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'; // Adicione se você estiver usando Reactive Forms
+import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterModule, NgIf],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.sass'
+})
+export class LoginComponent {
+  loginForm: FormGroup; 
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]], 
+      password: ['', [Validators.required, Validators.minLength(8)]] 
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Formulário enviado com sucesso!', this.loginForm.value);
+    } else {
+      console.log('O formulário contém erros.');
+      this.loginForm.markAllAsTouched(); 
+    }
+  }
+
+  onRegister(): void {
+    console.log('Navegar para a página de registro');
+    // this.router.navigate(['/register']); 
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+}
