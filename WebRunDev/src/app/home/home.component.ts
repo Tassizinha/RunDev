20a342cdf12fde7c88e36e53baf0dadb2a49bad7
Victoria Tassi <victoriatassi441@gmail.com>
2024-09-29 17:10:13 -0300

    Atualizações das pastas

---
+++ WebRunDev/src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass']
+})
+export class HomeComponent implements OnInit {
+
+
+
+
+ngOnInit(): void {
+  throw new Error('Method not implemented.');
+}
+  
+}
