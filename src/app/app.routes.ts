fb2f5f12f87c455f9d9bbc0a513e346a6c1ac833
Victoria Tassi <victoriatassi441@gmail.com>
2024-10-01 11:59:00 -0300

    Implementação das imagens, footer e alterações minimas no layout

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+
+export const routes: Routes = [
+   { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+   { path: 'login', component: LoginComponent },
+   { path: 'home', component: HomeComponent },
+   { path: '**', redirectTo: '/login' } 
+ ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+
+ export class AppRoutingModule { }
